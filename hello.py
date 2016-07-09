ff651e32f6209072a29edccfa3b3a8720aafed1a
= <=>
2016-07-09 17:55:35 +0800

    last version of routs and url_for

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, url_for
+#url_for generates urls programatically
 
+app = Flask(__name__)
+@app.route("/profile/<username>")
+def show_user_profile(username):
+    return "User: %s" % username
 @app.route("/")
-def hello_world():
-    return "Hello World!"
+def show_url_for():
+    return url_for('show_user_profile', username='jorge')
 if __name__ == '__main__':
+    app.debug = True
     app.run()
